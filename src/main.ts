dacc9878ed1467c0dc68703ef25355b091ca4aa4
yamaguchi_ss <BP004_yamaguchi@software-s.co.jp>
2024-02-06 15:03:37 +0900

    ２－２　３３％

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,32 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+// VueRouter の利用に必要なモジュールを読み込む
+import { RouteRecordRaw, createWebHistory, createRouter } from "vue-router";
+
+// トップページとゲームページを読み込む
+import TopPage from "./pages/TopPage.vue";
+import PlayPage from "./pages/PlayPage.vue";
+
+// 各画面のルーティング情報を記述する
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    name: "TopPage",
+    component: TopPage,
+  },
+  {
+    path: "/play",
+    name: "PlayPage",
+    component: PlayPage
+  },
+];
+
+// 各画面のルーティング情報からルーターを作成する
+const router = createRouter({
+  history: createWebHistory(),
+  routes: routes,
+});
+
+// `use` 関数を利用してルーターを使用可能にする
+createApp(App).use(router).mount('#app');
---
+++ src/pages/PlayPage.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <h1>プレイ画面</h1>
+  <h2>ユーザ名: {{ $route.query.name }}</h2>
+</template>
+
+<style>
+</style>
